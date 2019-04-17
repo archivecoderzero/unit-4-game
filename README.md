69c76837554b5db71e252e410f7106acd931ceff
unknown <archivecoderzero@gmail.com>
2019-04-16 19:45:50 -0700

    innitial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# unit-4-game
-Crystal Collector Game
+# Unit-4-Game : Crystal Collector :
+ 
+## This is the my submission for the unit 4 homework , View deployed website here : https://archivecoderzero.github.io/unit-4-game/.
+
+## Version 0.1 : 04/16/19
+> main iteration of the psuedocode in game.js
+
+
+
+
+
---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//psuedo code and stuff:
+
+//create global variable for the game , a var for the collected number.call it numCollect
+
+// generate a random number , looks like there aare 5 instances that we need a random number ,1. random number for the number that we need to compare at , 2 3 4 5 are for the assignment of the random number for all gems.
+
+// show the random number on the html file , this random number will be called "numCompare"
+
+// when there is an onclick event ,this pertains to the click of a gem , add that rumber to numCollect , 
+
+// create a function that compares the on click , this would be our for loop of when a numCollect > numCompare , ++ a score for wins , else , ++ scores on loss 
+
+
